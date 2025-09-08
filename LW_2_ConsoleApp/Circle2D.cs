0fd261e9de13a61e4638a4b95dc629a49caafff1
Dmitrii Liashko <dmitrii.liashko>
2025-09-09 00:25:37 +0300

    add TODO

---
+++ LW_2_ConsoleApp/Circle2D.cs
@@ -0,0 +1,19 @@
+namespace LW_2_ConsoleApp;
+
+/// <summary>
+/// Класс Circle2D представляет круг в двухмерном Евклидовом пространстве.
+/// </summary>
+public class Circle2D
+{
+    //TODO: реализовать класс Circle2D
+    //TODO: - конструктор Circle2D(Point2D center, double radius)
+    //TODO: --- следует соблюдать инвариант: радиус круга должен быть положительными
+    //TODO: - свойство double Diameter, возвращающее диаметр
+    //TODO: - свойство double Circumference, возвращающее длину окружности
+    //TODO: - свойство double Area, возвращающее площадь круга
+    //TODO: - метод double DistanceTo(Point2D p), возвращающий расстояние от данной точки до ближайшей точки окружности
+    //TODO: - метод double DistanceTo(Circle2D p), возвращающий расстояние между ближайшими друг к другу точками окружностей
+    //TODO: - метод bool Contains(Point2D p), проверяющий, лежит ли точка внутри круга
+    //TODO: - метод bool IntersectsWith(Circle2D other), определяющий, пересекаются ли два круга
+    //TODO: - метод bool Contains(Circle2D other), проверяющий, лежит ли другой круг полностью внутри этого круга
+}
---
+++ LW_2_ConsoleApp/Point2D.cs
@@ -0,0 +1,10 @@
+namespace LW_2_ConsoleApp;
+
+/// <summary>
+/// Структура Point2D представляет точку в двухмерном Евклидовом пространстве
+/// </summary>
+public struct Point2D
+{
+    //TODO: реализовать структуру Point2D
+    //TODO: Реализовать метод DistanceTo, вычисляющий евклидово расстояние до другой точки
+}

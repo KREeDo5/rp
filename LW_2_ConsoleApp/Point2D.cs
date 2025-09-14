32e0ae3303fd26d6cbdf2d966c2a65613b5dc654
Dmitrii Liashko <dmitrii.liashko>
2025-09-14 19:41:56 +0300

    Реализовал структуру Point2D. Подробнее о структурах: https://www.youtube.com/watch?v=8E9P_lnJ7RI&ab_channel=%23SimpleCode

--- LW_2_ConsoleApp/Point2D.cs
+++ LW_2_ConsoleApp/Point2D.cs
@@ -3,8 +3,24 @@ namespace LW_2_ConsoleApp;
 /// <summary>
 /// Структура Point2D представляет точку в двухмерном Евклидовом пространстве
 /// </summary>
-public struct Point2D
-{
-    //TODO: реализовать структуру Point2D
-    //TODO: Реализовать метод DistanceTo, вычисляющий евклидово расстояние до другой точки
+public readonly struct Point2D
+{   
+    public Point2D(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+    
+    public double X { get;  }
+    public double Y { get; }
+    
+    /// <summary>
+    /// Евклидово расстояние: sqrt((x1 - x2)^2 + (y1 - y2)^2)
+    /// Длина кратчайшей прямой линии между двумя точками в многомерном пространстве,
+    /// вычисляемая по обобщенной теореме Пифагора.
+    /// </summary>
+    public double DistanceTo(Point2D other)
+    {
+        return Math.Sqrt(Math.Pow(other.X - X, 2) + Math.Pow(other.Y - Y, 2));
+    }
 }

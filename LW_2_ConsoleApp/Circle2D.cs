783af10d199ce11edbeab18c1027b885b0582440
Dmitrii Liashko <dmitrii.liashko>
2025-09-14 20:27:22 +0300

    Diameter только возвращает значение, но с синт.сах

--- LW_2_ConsoleApp/Circle2D.cs
+++ LW_2_ConsoleApp/Circle2D.cs
@@ -20,19 +20,8 @@ public class Circle2D
     private double _radius;
  
     //TODO: - свойство double Diameter, возвращающее диаметр
-    public double Diameter
-    {
-        get => _radius * 2;
-        // set
-        // {
-        //     if (value < 0)
-        //     {
-        //         _radius = 0;
-        //         return;
-        //     }
-        //     _radius = value / 2;
-        // }
-    }
+    public double Diameter => _radius * 2;
+
     //TODO: - свойство double Circumference, возвращающее длину окружности
     //TODO: - свойство double Area, возвращающее площадь круга
     private double _circumference;

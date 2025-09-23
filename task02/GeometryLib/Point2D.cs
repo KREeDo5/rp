a9896a4f1399e247e98489ae92f7eab08387319e
Dmitrii Liashko <dmitrii.liashko>
2025-09-23 19:11:56 +0300

    Тесты класса Point2D

--- task02/GeometryLib/Point2D.cs
+++ task02/GeometryLib/Point2D.cs
@@ -5,17 +5,13 @@ namespace GeometryLib;
 /// </summary>
 public readonly struct Point2D(double x, double y)
 {
-    private double X { get;  } = x;
-    private double Y { get; } = y;
+    public double X { get;  } = x;
+    public double Y { get; } = y;
     
-    // // Максимальное отклонение, при котором расстояние всё ещё считается равным.
-    // public const double Tolerance = 1e-10;
-    //
-    // // Количество знаков после запятой у максимального отклонения.
-    // public const int Precision = 10;
+    // Максимальное отклонение, при котором расстояние всё ещё считается равным. (0.0001)
+    public const double Tolerance = 1e-4;
     
     /// <summary>
-    /// TODO: ТРЕБУЕТ ПРОВЕРКИ
     /// Евклидово расстояние: sqrt((x1 - x2)^2 + (y1 - y2)^2)
     /// Длина кратчайшей прямой линии между двумя точками в многомерном пространстве,
     /// вычисляемая по обобщенной теореме Пифагора.
@@ -24,13 +20,4 @@ public readonly struct Point2D(double x, double y)
     {
         return Math.Sqrt(Math.Pow(other.X - X, 2) + Math.Pow(other.Y - Y, 2));
     }
-    // public static bool operator ==(Point2D left, Point2D right) => left.Equals(right);
-    //
-    // public static bool operator !=(Point2D left, Point2D right) => left.Equals(right);
-    //
-    // public bool Equals(Point2D other)
-    // {
-    //     return Math.Abs(X - other.X) < Tolerance
-    //            && Math.Abs(Y - other.Y) < Tolerance;
-    // }
 }

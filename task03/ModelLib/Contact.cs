a003e76068db95be68a59a17df50fafcf9bb53fb
Dmitrii Liashko <dmitrii.liashko>
2025-10-01 01:40:04 +0300

    Добавил конструктор в Contact с необязательными параметрами, поправил некоторые свойства, реализовал частично метод SetPrimaryPhoneNumber

--- task03/ModelLib/Contact.cs
+++ task03/ModelLib/Contact.cs
@@ -7,30 +7,52 @@ public class Contact
     // 2. Контакт может иметь 0, 1 или несколько номеров телефона
     // - один из номеров всегда помечен как основной (primary), кроме случая, когда нет ни одного номера
 
-    //Контакт имеет свойства `string FirstName`, `string MiddleName` и `string LastName`
-    string FirstName; // Имя - не может быть пустой строкой
-    string MiddleName; // Отчество
-    string LastName; // Фамилия
+    private List<PhoneNumber> _phoneNumbers;
+
+    public Contact(String firstName, String middleName = "", String lastName = "")
+    {
+        if (String.IsNullOrEmpty(firstName.Trim())) //TODO: проверить на необходимость использования Trim
+        {
+            throw new ArgumentException("Имя - не может быть пустой строкой", nameof(firstName));
+        }
+
+        FirstName = firstName;
+        MiddleName = middleName;
+        LastName = lastName;
+        _phoneNumbers = new List<PhoneNumber>();
+    }
+    
+    // Имя - не может быть пустой строкой
+    public String FirstName { get; } 
+    // Отчество
+    public String MiddleName { get; } 
+    // Фамилия
+    public String LastName { get; }
 
     //- свойство `List<PhoneNumber> PhoneNumbers` возвращает все номера телефонов
     public List<PhoneNumber> PhoneNumbers => new List<PhoneNumber>(_phoneNumbers); // возврат списка копий
 
 
-    // - свойство `PhoneNumber? PrimaryPhoneNumber` возвращает основной номер телефона
+    // Возвращает основной номер телефона
     public PhoneNumber? PrimaryPhoneNumber => _primaryPhoneNumber;
+    
+    private PhoneNumber? _primaryPhoneNumber;
 
     // Удаляет номер телефона
     public void RemovePhoneNumber(PhoneNumber value)
     {
     }
-    
+
     // Добавляет номер телефона либо ничего не делает, если номер уже был
     public void AddPhoneNumber(PhoneNumber value)
     {
+        //TODO: проверка по хэшкоду, добавить equals в PhoneNumber
     }
-    
+
     // Меняет основной номер телефона
     public void SetPrimaryPhoneNumber(PhoneNumber value)
     {
+        //TODO: проверить, что номер есть в списке
+        _primaryPhoneNumber = value;
     }
 }

b4733d247543af2aba6c28cd72e14cbeb44c9ef4
Dmitrii Liashko <dmitrii.liashko>
2025-09-30 23:49:05 +0300

    замена типа int на long, проверка длины номера

--- task03/ModelLib/PhoneNumber.cs
+++ task03/ModelLib/PhoneNumber.cs
@@ -7,20 +7,8 @@ namespace ModelLib;
 /// </summary>
 public class PhoneNumber
 {
-    //     Метод ToString(), возвращающий строку номера телефона с символом + в начале
-    // Пример: +78362450272 — когда нет добавочного номера
-    // Пример: +12345678901x1234 — когда есть добавочный номер
-    
-    
-    
     public PhoneNumber(string text)
     {   
-        // Допускаются номера с символом + в начале и без него, например:
-        // +7 (8362) 45-02-72 → номер +78362450272
-        // 7 (8362) 45-02-72 → номер +78362450272
-        // 8 (8362) 45-02-72 → номер +88362450272
-        // Допускаются номера с добавочным (внутренним) номером после x, например:
-        // 1-234-567-8901 x1234 → номер +12345678901, добавочный 1234
         text = NormalizeText(text);
         if (text.Length == 0)
         {
@@ -31,17 +19,26 @@ public class PhoneNumber
             throw new ArgumentException("Недопустимый формат. Разрешены только цифры и один разделитель между ними.", nameof(text));
         }
         
-        // сохраняет отдельно основной номер и добавочный номер
+        // Сохраняет отдельно основной номер и добавочный номер
         string[] parts = text.Split(Separator, 2);
         if (parts[0] == "")
         {
             throw new ArgumentException("Номер телефона не может быть пустым", nameof(text));
         }
-        MainNumber = int.Parse(parts[0]);
-        
-        if (parts[1] != "")
+        // Проверка длины основного номера. Максимум 18 цифр (номера в Германии).
+        if (parts[0].Length > 18 )
         {
-            AdditionalNumber = int.Parse(parts[1]);
+            throw new ArgumentException("Недопустимая длина номера телефона", nameof(text));
+        }
+        MainNumber = long.Parse(parts[0]);
+        
+        if (parts.Length > 1 && parts[1] != "")
+        {
+            if (parts[1].Length > 18 )
+            {
+                throw new ArgumentException("Недопустимая длина добавочного номера телефона", nameof(text));
+            }
+            AdditionalNumber = long.Parse(parts[1]);
         }
     }
     
@@ -58,7 +55,7 @@ public class PhoneNumber
         if (string.IsNullOrEmpty(text))
             return text;
         
-        // удаляет пробелы, дефисы, круглые скобки
+        // Удаляет пробелы, дефисы, круглые скобки
         text = Regex.Replace(text, @"[\s\-\(\)]", "");
 
         if (text.StartsWith("+"))
@@ -74,12 +71,16 @@ public class PhoneNumber
     
     private const Char Separator = 'x';
     
-    private int MainNumber { get; }
-    private int? AdditionalNumber { get; }
+    private long MainNumber { get; }
+    private long? AdditionalNumber { get; }
     
-    // Свойство string Number, возвращающее строку номера телефона с символом + в начале, но без добавочного номера
+    // Возвращает строку номера телефона с символом + в начале, но без добавочного номера
     public String Number => "+" + MainNumber ;
     
-    // Свойство string Ext, возвращающее добавочный номер либо пустую строку
+    // Возвращает добавочный номер либо пустую строку
     public String Ext => AdditionalNumber?.ToString() ?? "" ;
+    
+    // TODO: Метод ToString(), возвращающий строку номера телефона с символом + в начале
+    // Пример: +78362450272 — когда нет добавочного номера
+    // Пример: +12345678901x1234 — когда есть добавочный номер
 }

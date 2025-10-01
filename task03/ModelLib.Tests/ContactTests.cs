b40b2539c50c638e3643c8c070192f740e008ce8
Dmitrii Liashko <dmitrii.liashko>
2025-10-01 04:02:24 +0300

    Основные тесты инициализации модели

--- task03/ModelLib.Tests/ContactTests.cs
+++ task03/ModelLib.Tests/ContactTests.cs
@@ -2,5 +2,23 @@ namespace ModelLib.Tests;
 
 public class ContactTests
 {
-    //TODO: Тесты для Contact
+    /// Тест инициализации контакта телефона с пустым именем
+    [Fact]
+    public void Cannot_Init_Contact()
+    {
+        Assert.Throws<ArgumentException>(() => new Contact(""));
+        Assert.Throws<ArgumentException>(() => new Contact("    "));
+    }
+    
+    /// Тест инициализации контакта с лишними пробелами
+    [Fact]
+    public void Correct_Init_Contact()
+    {
+        Contact value = new Contact("Иван", "Петрович    ", "    Сидоров");
+        Assert.Equal("Иван", value.FirstName);
+        Assert.Equal("Петрович", value.MiddleName);
+        Assert.Equal("Сидоров", value.LastName);
+        Assert.Empty(value.PhoneNumbers);
+        Assert.Null(value.PrimaryPhoneNumber);
+    }
 }
